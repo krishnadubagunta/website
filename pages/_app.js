bd16216519e61bccbdf8bbe7c56cd188fefdb91c
Krishna Dubagunta <dubagunta.saikrishna@outlook.com>
2021-09-28 15:51:16 -0400

    add title and favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import { ThemeProvider } from '@material-ui/core'
 import { StyledEngineProvider } from '@material-ui/core/styles'
 import '../styles/globals.css'
@@ -5,15 +6,27 @@ import { theme } from '../styles/theme'
 import { AnimatePresence } from 'framer-motion'
 
 function MyApp({ Component, pageProps, router }) {
-  return <div style={{ padding: '8px' }}>
-    <StyledEngineProvider injectFirst>
-      <ThemeProvider theme={theme}>
-        <AnimatePresence exitBeforeEnter>
-          <Component {...pageProps} key={router.route} />
-        </AnimatePresence>
-      </ThemeProvider>
-    </StyledEngineProvider>
-  </div>
+  return <>
+    <Head>      
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="manifest" href="/site.webmanifest" />
+      <meta name="apple-mobile-web-app-title" content="- kd photography" />
+      <meta name="application-name" content="- kd photography" />
+      <meta name="msapplication-TileColor" content="#da532c" />
+      <meta name="theme-color" content="#ffffff" />
+    </Head>
+    <div style={{ padding: '8px' }}>
+      <StyledEngineProvider injectFirst>
+        <ThemeProvider theme={theme}>
+          <AnimatePresence exitBeforeEnter>
+            <Component {...pageProps} key={router.route} />
+          </AnimatePresence>
+        </ThemeProvider>
+      </StyledEngineProvider>
+    </div>
+  </>
 }
 
 export default MyApp

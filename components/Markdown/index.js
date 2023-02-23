55d2acab047b28e10044636810e2bebb17d112d8
Krishna Dubagunta <dubagunta.saikrishna@outlook.com>
2023-02-23 18:30:42 -0500

    fix links and revalidate

--- components/Markdown/index.js
+++ components/Markdown/index.js
@@ -5,9 +5,7 @@ export default function Markdown({ children }) {
     return <ReactMarkdown className="text-sm text-gray-400" components={{
         h1: function H1({ children: c }) { return <p className="text-lg">{c}</p> },
         p: function p({ children: c }) { return <p className="text-sm text-gray-400 pb-4">{ c }</p> },
-        a: function a({ children: c, href }) { return <span>
-            <Link  href={href} target="_blank" className="text-blue-500" >&nbsp;{ c }</Link>
-        </span> },
+        a: function a({ children: c, href }) { return <Link  href={href} target="_blank" className="text-blue-500" >&nbsp;{ c }</Link> },
     }}>
         { children }
     </ReactMarkdown>

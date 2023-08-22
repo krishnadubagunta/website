036048e67172598e5626a836aa2d0beff55909be
Krishna Dubagunta <dubagunta.saikrishna@outlook.com>
2023-08-22 09:59:10 -0400

    build fixes

--- apps/portfolio/app/loading.tsx
+++ apps/portfolio/app/loading.tsx
@@ -3,7 +3,7 @@ import TypographyH3 from 'kd-ui/ui/typography/h3';
 
 export default function Loading() {
     return (<article className='pt-6 flex flex-col space-y-4 w-10/12'>
-    <TypographyH3>hello, i'm krishna dubagunta 👋🏼</TypographyH3>
+    <TypographyH3>hello, i&apos;m krishna dubagunta 👋🏼</TypographyH3>
     <Skeleton className='h-screen w-full' />
   </article>);
   }

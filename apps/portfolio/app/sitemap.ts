d2d83c6e697e6fa4b5c2f5a495a36984f22e6bab
Krishna Dubagunta <dubagunta.saikrishna@outlook.com>
2023-08-23 10:57:52 -0400

    position change

--- apps/portfolio/app/sitemap.ts
+++ apps/portfolio/app/sitemap.ts
@@ -2,11 +2,11 @@ import { allBlogs } from 'contentlayer/generated';
 
 export default async function sitemap() {
   const blogs = allBlogs.map((post) => ({
-    url: `https://krishnadubagunta.com/blog/${post.slug}`,
+    url: `https://krishnadubagunta.com/blogs/${post.slug}`,
     lastModified: post.publishedAt,
   }));
 
-  const routes = ['', '/blog'].map(
+  const routes = ['', '/blogs','/gallery'].map(
     (route) => ({
       url: `https://krishnadubagunta.com${route}`,
       lastModified: new Date().toISOString().split('T')[0],

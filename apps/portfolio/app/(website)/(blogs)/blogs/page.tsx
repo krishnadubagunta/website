e3455730908fe72ff8dd394c0651a5d6f38ca948
Krishna Dubagunta <dubagunta.saikrishna@outlook.com>
2024-08-12 23:55:46 +0530

    redo loading

--- apps/portfolio/app/(website)/(blogs)/blogs/page.tsx
+++ apps/portfolio/app/(website)/(blogs)/blogs/page.tsx
@@ -1,7 +1,7 @@
 import { allBlogs } from "contentlayer/generated";
 import Link from "next/link";
 import Small from "kd-ui/ui/typography/small";
-import { formatDate } from "../_lib/string";
+import { formatDate } from "../../_lib/string";
 import { Metadata } from "next";
 import TypographyH2 from "kd-ui/ui/typography/h2";
 import TypographyH3 from "kd-ui/ui/typography/h3";
@@ -22,12 +22,7 @@ export default function Page() {
       <TypographyH3>read my blog</TypographyH3>
       <div className="pt-4">
         {allBlogs
-          .sort((a, b) => {
-            if (new Date(a.publishedAt) > new Date(b.publishedAt)) {
-              return -1;
-            }
-            return 1;
-          })
+          .sort((a, b) => new Date(a.publishedAt) > new Date(b.publishedAt) ? -1 : 1)
           .filter((blog) => !blog.hide)
           .map((post) => (
             <section className="pb-4" key={post.slug}>

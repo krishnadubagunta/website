dbb7512d26fde442463b515c31fdfd58b8615531
Krishna Dubagunta <dubagunta.saikrishna@outlook.com>
2024-11-11 23:59:51 +0530

    website sitemap

--- apps/portfolio/app/(website)/sitemap.ts
+++ apps/portfolio/app/(website)/sitemap.ts
@@ -1,17 +1,11 @@
-import { allBlogs } from 'contentlayer/generated';
 
 export default async function sitemap() {
-  const blogs = allBlogs.map((post) => ({
-    url: `https://krishnadubagunta.com/blogs/${post.slug}`,
-    lastModified: post.publishedAt,
-  }));
-
-  const routes = ['', '/blogs','/gallery'].map(
+  const routes = ['', '/blog','/gallery'].map(
     (route) => ({
       url: `https://krishnadubagunta.com${route}`,
       lastModified: new Date().toISOString().split('T')[0],
     })
   );
 
-  return [...routes, ...blogs];
+  return routes;
 }

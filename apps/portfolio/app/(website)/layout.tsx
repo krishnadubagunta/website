a523783d784dc442957aa3fa9ffaea45b04094da
Krishna Dubagunta <dubagunta.saikrishna@outlook.com>
2024-12-01 04:29:56 +0530

    Light theme

--- apps/portfolio/app/(website)/layout.tsx
+++ apps/portfolio/app/(website)/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
           "sm:px-12 lg:px-20 px-4 py-6"
         )}
       >
-        <ThemeProvider defaultTheme="dark" enableSystem enableColorScheme>
+        <ThemeProvider defaultTheme="light" enableColorScheme>
           <Navbar
             refs={{
               "/": {

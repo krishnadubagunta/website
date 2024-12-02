75290efbb2e3e2154be4b3af0c8461b5d974322e
Krishna Dubagunta <dubagunta.saikrishna@outlook.com>
2024-12-02 14:48:51 +0530

    Home Content Social Change

--- packages/shared/kd-ui/ui/social.tsx
+++ packages/shared/kd-ui/ui/social.tsx
@@ -4,17 +4,17 @@ import { Card, CardContent } from "./card";
 import Small from "./typography/small";
 import { ReactChildren } from "./types";
 
-export default function SocialLink({ href, children, username }: ReactChildren & { href: string, username: string}) {
+export default function SocialLink({ href, children }: ReactChildren & { href: string }) {
   return <Link href={href} target="_blank">
-  <Card className="w-fit border-primary flex items-center m-2">
+  <Card className="w-fit flex items-center m-2">
     <CardContent className="p-2 flex items-center">
-      <div className="flex justify-center items-center rounded-full h-10 w-10 border border-primary p-2 ">
+      <div className="flex justify-center items-center h-10 w-10 p-2 ">
         { children }
       </div>
-      <Small className=" px-2">{username}</Small>
+      {/* <Small className=" px-2">{username}</Small>
       <div className="p-1 h-5 w-5 flex justify-center items-center ">
         <ExternalLink />
-      </div>
+      </div> */}
     </CardContent>
   </Card>
 </Link>;

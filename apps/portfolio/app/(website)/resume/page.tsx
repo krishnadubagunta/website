ac4ca24d597a455808271f90a84488f55fef7eaa
Krishna Dubagunta <dubagunta.saikrishna@outlook.com>
2025-05-07 22:44:43 -0500

    metadata refresh

--- apps/portfolio/app/(website)/resume/page.tsx
+++ apps/portfolio/app/(website)/resume/page.tsx
@@ -2,11 +2,11 @@ import { Metadata } from "next";
 import Content from "./content.mdx";
 
 export const metadata: Metadata = {
-    title: "Resume",
-    description: "Resume of Saikrishna Dubagunta",
+    title: "Sai Krishna Dubagunta's Resume",
+    description: "Results-driven Principal Consultant Architect with over 8 years of extensive experience designing and delivering high-performance, scalable full-stack solutions. Expert in frontend technologies (ReactJS, NextJS, Angular) and backend development (Java, Golang, Ruby, Python, Node.js). Proven success in API design, microservices architecture, performance optimization, and team leadership. Passionate about mentoring engineering teams and driving business outcomes through technology innovation.",
     openGraph: {
-        title: 'Resume',
-        description: 'Resume of Saikrishna Dubagunta',
+        title: "Sai Krishna Dubagunta's Resume",
+        description: 'Results-driven Principal Consultant Architect with over 8 years of extensive experience designing and delivering high-performance, scalable full-stack solutions. Expert in frontend technologies (ReactJS, NextJS, Angular) and backend development (Java, Golang, Ruby, Python, Node.js). Proven success in API design, microservices architecture, performance optimization, and team leadership. Passionate about mentoring engineering teams and driving business outcomes through technology innovation.',
         url: 'https://www.krishnadubagunta.com/resume',
         type: 'profile',
         gender: 'male',
@@ -22,7 +22,7 @@ export const metadata: Metadata = {
         emails: ['dubagunta.saikrishna@outlook.com'],
       },
     appleWebApp: {
-        title: 'Resume',
+        title: "Sai Krishna Dubagunta's Resume",
         statusBarStyle: 'black-translucent',
         startupImage: {
             url: "https://krishnadubagunta.com/images/me.jpg",

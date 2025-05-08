5fe8df81c00c43393d03a07561d1b52a225b2830
Krishna Dubagunta <dubagunta.saikrishna@outlook.com>
2025-05-07 22:41:19 -0500

    metadata

--- apps/portfolio/app/(website)/resume/page.tsx
+++ apps/portfolio/app/(website)/resume/page.tsx
@@ -1,4 +1,35 @@
+import { Metadata } from "next";
 import Content from "./content.mdx";
+
+export const metadata: Metadata = {
+    title: "Resume",
+    description: "Resume of Saikrishna Dubagunta",
+    openGraph: {
+        title: 'Resume',
+        description: 'Resume of Saikrishna Dubagunta',
+        url: 'https://www.krishnadubagunta.com/resume',
+        type: 'profile',
+        gender: 'male',
+        images: [
+          {
+            url: "https://krishnadubagunta.com/images/me.jpg",
+            alt: 'Me near DUMBO in brooklyn just after snow',
+            type: 'image/jpg'
+          }
+        ],
+        firstName: 'Sai Krishna',
+        lastName: 'Dubagunta',
+        emails: ['dubagunta.saikrishna@outlook.com'],
+      },
+    appleWebApp: {
+        title: 'Resume',
+        statusBarStyle: 'black-translucent',
+        startupImage: {
+            url: "https://krishnadubagunta.com/images/me.jpg",
+            media: 'image/jpg',
+        }
+    },
+}
 
 export default function Resume() {
   return (<>
